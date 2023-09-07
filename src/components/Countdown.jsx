543364a912a3dcdcd973d5b0d3c321ba13ef11ec
fensami <fensami2323@gmail.com>
2023-09-07 23:09:07 +0600

    countdown

--- src/components/Countdown.jsx
+++ src/components/Countdown.jsx
@@ -1,87 +1,76 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 
-function Countdown() {
-  const targetDate = new Date();
-  targetDate.setMonth(targetDate.getMonth() + 4);
+function Countdown({ targetDate }) {
+  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
 
-  const calculateTimeRemaining = () => {
-    const currentTime = new Date();
-    const timeDifference = targetDate - currentTime;
+  function calculateTimeLeft() {
+    const now = new Date();
+    const difference = new Date(targetDate) - now;
 
-    if (timeDifference <= 0) {
-      return {
-        months: 0,
-        days: 0,
-        hours: 0,
-        minutes: 0,
-        seconds: 0,
-      };
+    if (difference <= 0) {
+      return { days: 0, hours: 0, minutes: 0, seconds: 0 };
     }
 
-    // Calculate days, hours, minutes, and seconds
-    const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
-    const hours = Math.floor((timeDifference / (1000 * 60 * 60)) % 24);
-    const minutes = Math.floor((timeDifference / (1000 * 60)) % 60);
-    const seconds = Math.floor((timeDifference / 1000) % 60);
+    const days = Math.floor(difference / (1000 * 60 * 60 * 24));
+    const hours = Math.floor(
+      (difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
+    const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((difference % (1000 * 60)) / 1000);
 
-    return { months: 4, days, hours, minutes, seconds };
-  };
-
-  const [timeRemaining, setTimeRemaining] = useState(calculateTimeRemaining());
+    return { days, hours, minutes, seconds };
+  }
 
   useEffect(() => {
-    const timerInterval = setInterval(() => {
-      const newTimeRemaining = calculateTimeRemaining();
-      setTimeRemaining(newTimeRemaining);
-
-      if (
-        newTimeRemaining.months === 0 &&
-        newTimeRemaining.days === 0 &&
-        newTimeRemaining.hours === 0 &&
-        newTimeRemaining.minutes === 0 &&
-        newTimeRemaining.seconds === 0
-      ) {
-        clearInterval(timerInterval);
-      }
+    const timer = setInterval(() => {
+      setTimeLeft(calculateTimeLeft());
     }, 1000);
 
-    return () => {
-      clearInterval(timerInterval);
-    };
-  }, []);
+    return () => clearInterval(timer);
+  }, [targetDate]); // Add targetDate as a dependency
 
   return (
-    <div className='bg-black'>
-      <h1 className='text-center font-semibold md:text-5xl text-white py-5'>
-        {timeRemaining.months === 0 &&
-        timeRemaining.days === 0 &&
-        timeRemaining.hours === 0 &&
-        timeRemaining.minutes === 0 &&
-        timeRemaining.seconds === 0
+    <div className="bg-black py-5 font-mono">
+      <h1 className="text-center font-semibold md:text-5xl text-xl text-white py-5">
+        {timeLeft.days === 0 &&
+        timeLeft.hours === 0 &&
+        timeLeft.minutes === 0 &&
+        timeLeft.seconds === 0
           ? 'Countdown Expired'
-          : 'We Will Comming Soon...'}
+          : 'We are coming in'}
       </h1>
-      {timeRemaining.months === 0 &&
-      timeRemaining.days === 0 &&
-      timeRemaining.hours === 0 &&
-      timeRemaining.minutes === 0 &&
-      timeRemaining.seconds === 0 ? null : (
-        <div className='flex justify-between items-center bg-black md:rounded-b-lg md:w-2/3 mx-auto'>
-          <div className='bg-black py-2 px-3 text-white font-bold text-3xl'>
-            <span>Months: {timeRemaining.months}</span>
+      {timeLeft.days === 0 &&
+      timeLeft.hours === 0 &&
+      timeLeft.minutes === 0 &&
+      timeLeft.seconds === 0 ? null : (
+        <div className="md:flex my-5 justify-center grid grid-cols-2 gap-5 overflow-auto items-center bg-black md:rounded-b-lg  mx-auto px-5">
+          <div className="flex flex-col py-5 px-7 bg-neutral rounded-box text-xl font-semibold text-neutral-content">
+            <div className="text-center">
+              <span>{timeLeft.days}</span>
+              <p>Days</p>
+            </div>
           </div>
-          {/* <div className='py-2 px-3 text-white font-bold md:text-3xl'>
-            <span>Days: {timeRemaining.days}</span>
-          </div> */}
-          <div className='py-2 px-3 text-white font-bold md:text-3xl'>
-            <span>Hours: {timeRemaining.hours}</span>
+
+          <div className="flex flex-col py-5 px-7 bg-neutral rounded-box text-xl font-semibold text-neutral-content">
+            <div className="text-center">
+              <span>{timeLeft.hours}</span>
+              <p>Hours</p>
+            </div>
           </div>
-          <div className='py-2 px-3 text-white font-bold md:text-3xl'>
-            <span>Minutes: {timeRemaining.minutes}</span>
+
+          <div className="flex flex-col py-5 px-7 bg-neutral rounded-box text-xl font-semibold text-neutral-content">
+            <div className="text-center">
+              <span>{timeLeft.minutes}</span>
+              <p>Minutes</p>
+            </div>
           </div>
-          {/* <div className='py-2 px-3 text-white font-bold md:text-3xl'>
-            <span>Seconds: {timeRemaining.seconds}</span>
-          </div> */}
+
+          <div className="flex flex-col py-5 px-7 bg-neutral rounded-box text-xl font-semibold text-neutral-content">
+            <div className="text-center">
+              <span>{timeLeft.seconds}</span>
+              <p>Seconds</p>
+            </div>
+          </div>
         </div>
       )}
     </div>
